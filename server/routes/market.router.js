7f8416771216cbd1bb26a2f651ae4384925df6d3
Kris Szafranski <kdszafranski@gmail.com>
2017-09-06 10:55:35 -0500

    fixed typo

--- server/routes/market.router.js
+++ server/routes/market.router.js
@@ -14,7 +14,7 @@ var router = express.Router();
 // As a stretch goal, move this to the database
 var marketItems = [
   {id: 1, name: 'Apple', cost: 0.99},
-  {id: 2, name: 'Tomatoe', cost: 1.19},
+  {id: 2, name: 'Tomato', cost: 1.19},
   {id: 3, name: 'Coffee', cost: 2.99},
   {id: 4, name: 'Flowers', cost: 8.99},
   {id: 5, name: 'Orange', cost: 0.89},
