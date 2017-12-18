c9e9a457a750b3a66a39e6e104a324dbf9bafd13
Mike Pettman <mp_palmatum@yahoo.com>
2017-12-17 23:26:16 -0600

    added angular-timer directive, bower

--- server/public/scripts/controllers/market.controller.js
+++ server/public/scripts/controllers/market.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('MarketController', ['UserService', '$interval', function (UserService, $interval) {
+myApp.controller('MarketController', ['UserService', '$interval', '$scope', function (UserService, $interval, $scope) {
   // console.log('MarketController created');
   var vm = this;
   vm.userService = UserService;
@@ -41,10 +41,17 @@ myApp.controller('MarketController', ['UserService', '$interval', function (User
     5: 0.00,
     6: 0.00,
   };
+  vm.gameDuration = 0;
+  vm.gameOn = true;
 
   // get new prices every 8 seconds
   vm.getMarketItems = function () {
-    UserService.getMarket();
+    vm.gameDuration++;
+    if (vm.gameDuration > 15) {
+      vm.gameOn = false;
+    } else {
+      UserService.getMarket();
+    }
     // console.log('controller market items: ', vm.marketItems);
   };
 
@@ -53,46 +60,64 @@ myApp.controller('MarketController', ['UserService', '$interval', function (User
 
   vm.buyFruit = function (itemCost, id) {
     // stop buy if not enough money
-    if (itemCost > vm.userObject.money) {
-      return;
-    } else {
-      //deduct money from wallet
-      vm.userObject.money = vm.userObject.money - itemCost;
-      // increment quantity and total of given fruit
-      vm.quantity[id]++;
-      vm.total[id]++;
-      // add to sum for item, update average
-      vm.sum[id] += itemCost;
-      vm.average[id] = vm.sum[id] / vm.total[id];
+    if (vm.gameOn) {
+      if (itemCost > vm.userObject.money) {
+        return;
+      } else {
+        //deduct money from wallet
+        vm.userObject.money = vm.userObject.money - itemCost;
+        // increment quantity and total of given fruit
+        vm.quantity[id]++;
+        vm.total[id]++;
+        // add to sum for item, update average
+        vm.sum[id] += itemCost;
+        vm.average[id] = vm.sum[id] / vm.total[id];
+      }
     }
   };
 
   vm.sellFruit = function (itemCost, id) {
     // stop sell if fruit count is 0
-    if (vm.quantity[id] == 0) {
-      return;
-    } else {
-      vm.userObject.money = vm.userObject.money + itemCost;
-      // decrement quantity of given fruit
-      vm.quantity[id]--;
-    }
-    if (vm.userObject.money > vm.scoreObject.data[0].score){
-      vm.postScore(vm.userObject.money);
+    if (vm.gameOn) {
+      if (vm.quantity[id] == 0) {
+        return;
+      } else {
+        vm.userObject.money = vm.userObject.money + itemCost;
+        // decrement quantity of given fruit
+        vm.quantity[id]--;
+      }
+      if (vm.userObject.money > vm.scoreObject.data[0].score) {
+        vm.postScore(vm.userObject.money);
+      }
     }
   };
 
-  vm.postScore = function(score){
+  vm.postScore = function (score) {
     console.log('Score: ', score);
     UserService.postScore(score);
     vm.showScore();
   };
 
-  vm.showScore = function(){
+  vm.showScore = function () {
     // console.log('ShowScore func called');
     UserService.getScore();
   };
 
   // call on page load
   vm.showScore();
+
+
+  $scope.timerRunning = true;
+  $scope.startTimer = function () {
+    $scope.$broadcast('timer-start');
+    $scope.timerRunning = true;
+  };
+  $scope.stopTimer = function () {
+    $scope.$broadcast('timer-stop');
+    $scope.timerRunning = false;
+  };
+  $scope.$on('timer-stopped', function (event, data) {
+    console.log('Timer Stopped - data = ', data);
+  });
 
 }]);
